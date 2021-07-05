130dea592bf3e8b67c3915b7fa4bb44dcfcd35e9
Mustafa Abdelhai <hi@mustafa.li>
2021-07-05 17:38:57 +0200

    env changes

--- sidebars.js
+++ sidebars.js
@@ -3,7 +3,7 @@ module.exports = {
     General: ['home', 'faqs'],
     ["Deta Base"]: ['base/about', 'base/sdk', 'base/http', 'base/base_ui', 'base/node_tutorial', 'base/py_tutorial'],
     ["Deta Drive"]: ['drive/about', 'drive/http', 'drive/sdk', 'drive/py_tutorial', 'drive/node_tutorial'],
-    ["Deta Micros"]: ['micros/about', 'micros/getting_started', 'micros/run', 'micros/cron', 'micros/custom_domains', 'micros/env', 'micros/pre-set', 'micros/api_keys', 'micros/visor', 'micros/faqs_micros'],
+    ["Deta Micros"]: ['micros/about', 'micros/getting_started', 'micros/run', 'micros/cron', 'micros/custom_domains', 'micros/env', 'micros/pre_set', 'micros/api_keys', 'micros/visor', 'micros/faqs_micros'],
     ["Deta CLI"]: ['cli/install', 'cli/auth', 'cli/commands', 'cli/releases'],
     ["Quick Start"]: ['tutorials/fast-api-guide', 'tutorials/webhooks-guide', 'tutorials/cron-guide', 'tutorials/form-endpoints-guide', 'tutorials/static-files-guide'],
   },
