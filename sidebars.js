fedc871cf3b3256ba6f8d9341b2452f5acdce6f7
Mustafa Abdelhai <hi@mustafa.li>
2021-08-04 15:37:40 +0200

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -3,7 +3,7 @@ module.exports = {
     General: ['home', 'faqs', 'common_issues'],
     ["Deta Base"]: ['base/about', 'base/sdk', 'base/http', 'base/base_ui', 'base/node_tutorial', 'base/py_tutorial'],
     ["Deta Drive"]: ['drive/about', 'drive/http', 'drive/sdk', 'drive/py_tutorial', 'drive/node_tutorial'],
-    ["Deta Micros"]: ['micros/about', 'micros/getting_started', 'micros/deploy', 'micros/run', 'micros/cron', 
+    ["Deta Micros"]: ['micros/about', 'micros/getting_started', 'micros/deploy', 'micros/cron', 
     'micros/custom_domains', 'micros/env_vars', 'micros/api_keys', 'micros/visor', 'micros/deploy_to_deta_button', 'micros/faqs_micros'],
     ["Deta CLI"]: ['cli/install', 'cli/auth', 'cli/commands', 'cli/releases'],
     ["Quick Start"]: ['tutorials/fast-api-guide', 'tutorials/webhooks-guide', 'tutorials/cron-guide', 'tutorials/form-endpoints-guide'],
